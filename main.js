ba43b0ac3db6659a80687ae47ea3e3e4eb51b96e
Marta Nicoli <marta.nicoli@gmail.com>
2022-11-17 21:49:31 -0300

    Agregar corregido

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let productos =[];
+let listaProductos =[];
 
 const objProducto ={
+    id:'',
     nombre:'',
     cantidad:''
 }
@@ -14,14 +15,18 @@ const btnAgregar= document.querySelector('#btnAgregar');
 
 formulario.addEventListener('submit', validarFormulario);//cuando detecte el submit valida el formulario.
 
-function validarFormulario (event) {
-    event.preventDefault();//Este event es para que no se ejecute de forma automatica.
+function validarFormulario (e) {
+    e.preventDefault();//Este event es para que no se ejecute de forma automatica.
      
+
+    //valida que los campos no esten vacios
     if(nombreInput.value === '' || cantidadInput.value === ''){
         alert('Todos los campos son obligatorios');
-        return;
+        return;// Con el return sale de la función.
     }
 
+
+// Valida si debe editar o agregar los datos.
     if (editando){
         //editarProducto();
         editando= false;
@@ -32,29 +37,54 @@ function validarFormulario (event) {
         objProducto.cantidad = cantidadInput.value;
 
         agregarProducto();
-        listaProductos.push({...objProducto});
+    }
+}
 
+function agregarProducto(){
+
+        listaProductos.push({...objProducto});//Por medio del push agregara el objeto producto.
         mostrarProducto();
-        
+       
     }
 
+    
 function mostrarProducto() {
-    const insumos = document.querySelector ('.insumos');
+    const divProductos = document.querySelector ('.div-productos');
 
-    listaProductos.forEach(producto => {
-        const {nombre, cantidad} = empleado;
+    // Recorremos la lista de productos
+        listaProductos.forEach(producto => {
+        const {id, nombre, cantidad} = producto;
+
         
+        const parrafo = document.createElement('p');
+        parrafo.textContent =`${id} - ${nombre} - ${cantidad} -`;
+        parrafo.dataset.id = id;// ayuda identifica el elemento a modificar o eliminar.
+
+
+        const editarBoton = document.createElement('button');
+        //editarBoton.onclick = () cargarProducto(producto);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-editar');
+        parrafo.append(editarBoton);
+
+        const eliminarBoton = document.createElement('button');
+        //eliminarBoton.onclick = () cargarProducto(producto);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-eliminar');
+        parrafo.append(eliminarBoton);
+
+        const hr = document.createElement('hr');
+
+        divProductos.appendChild(parrafo); //agregar como nodo hijo del contenedor 
+        divProductos.appendChild(hr);
+
     });
-    
-    
-    
-    
-
-
 
 }
+    
+    
+
+    
 
 
-}
 
-function agregarInsumos()
